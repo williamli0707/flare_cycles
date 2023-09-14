7af557c15c7bdfeb1b704f4cc371ead84dd3a104
William Li <williamhli0707@gmail.com>
2023-09-13 17:32:36 -0700

    accidental wrong input

--- plot_multiple_bin_sizes.py
+++ plot_multiple_bin_sizes.py
@@ -5,7 +5,7 @@ from flare_model_time_binning import main
 
 bin_sizes = [2, 10, 20, 50]
 for i in bin_sizes:
-    inputs, outputs, model, history = main(bin_sizes, 100)
+    inputs, outputs, model, history = main(i, 100)
     plt.plot(history.history['accuracy'])
     plt.plot(history.history['val_accuracy'])
 
