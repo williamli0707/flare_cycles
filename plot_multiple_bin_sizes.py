0602cf8d01a374b12a76d68673eec37c98a99298
William Li <williamhli0707@gmail.com>
2023-09-13 17:23:50 -0700

    fixed plotting

--- plot_multiple_bin_sizes.py
+++ plot_multiple_bin_sizes.py
@@ -8,7 +8,9 @@ for i in bin_sizes:
     inputs, outputs, model, history = main(bin_sizes, 100)
     plt.plot(history.history['accuracy'])
     plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'val'], loc='upper left')
+
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend([('train_' + str(i)) for i in bin_sizes] + [('val' + str(i)) for i in bin_sizes], loc='upper left')
+plt.show()
