340d9cd3956e26fc5d104ac174b497fe83de9a31
William Li <williamhli0707@gmail.com>
2023-09-14 22:35:53 -0700

    fixed plotting issues, removed extraneous files

--- plot_multiple_bin_sizes.py
+++ plot_multiple_bin_sizes.py
@@ -6,11 +6,11 @@ from flare_model_time_binning import main
 bin_sizes = [2, 10, 20, 50]
 for i in bin_sizes:
     inputs, outputs, model, history = main(i, 100)
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
 
 plt.title('model accuracy')
-plt.ylabel('accuracy')
+plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend([('train_' + str(i)) for i in bin_sizes] + [('val' + str(i)) for i in bin_sizes], loc='upper left')
 plt.show()
