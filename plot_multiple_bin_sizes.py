6172c53caf9040b5f265ad4138e09c3fe048bdec
William Li <williamhli0707@gmail.com>
2023-09-15 22:02:55 -0700

    keep results

--- plot_multiple_bin_sizes.py
+++ plot_multiple_bin_sizes.py
@@ -4,8 +4,10 @@ import pandas as pd
 from flare_model_time_binning import main
 
 bin_sizes = [2, 10, 20, 50]
+results = []
 for i in bin_sizes:
     inputs, outputs, model, history = main(i, 100)
+    results.append((inputs, outputs, model, history))
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
     cont = input("continue? (y/n)")
